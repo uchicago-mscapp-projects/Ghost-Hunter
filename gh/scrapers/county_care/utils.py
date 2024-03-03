abd741dce9dea245868942ca908e0b1bc5c80382
Jibbie17 <gabesbarrett17@gmail.com>
2024-03-03 05:10:45 -0600

    finished 2nd round scraper but site is down

--- gh/scrapers/county_care/utils.py
+++ gh/scrapers/county_care/utils.py
@@ -1,12 +1,19 @@
+import random
 import time
 import requests
 from urllib.parse import urlparse
 
 ALLOWED_DOMAINS = ("https://countycare",)
 
-REQUEST_DELAY = 2
+REQUEST_DELAY = 3
 
-HEADERS = {'User-Agent':"Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:123.0) Gecko/20100101 Firefox/123.0"}
+HEADERS1 = {'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_2) AppleWebKit/601.3.9 (KHTML, like Gecko) Version/9.0.2 Safari/601.3.9'}
+HEADERS2 = {'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246"}
+USER_AGENTS = ['Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_2) AppleWebKit/601.3.9 (KHTML, like Gecko) Version/9.0.2 Safari/601.3.9',
+"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246",
+"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246",
+"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.111 Safari/537.36",
+"Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:15.0) Gecko/20100101 Firefox/15.0.1"]
 
 def make_post(url, dictionary):
     """
@@ -15,17 +22,23 @@ def make_post(url, dictionary):
     This function ensure that the domain matches what is expected and that the rate limit
     is obeyed.
     """
+    
+    delay = REQUEST_DELAY + round(5*(random.random()), 2)
+    index = random.randrange(0,5,1)
+    headers = {'User-Agent': USER_AGENTS[index]}
+
+
     # check if URL starts with an allowed domain name
     for domain in ALLOWED_DOMAINS:
         if url.startswith(domain):
             break
     else:
         raise ValueError(f"can not fetch {url}, must be in {ALLOWED_DOMAINS}")
-    time.sleep(REQUEST_DELAY)
+    time.sleep(delay)
     print(f"Fetching {url}")
     
 
-    r = requests.post(url, json=dictionary)
+    r = requests.post(url, json=dictionary, headers=headers)
     
     return r
 
