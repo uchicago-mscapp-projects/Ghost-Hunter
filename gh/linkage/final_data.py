317307fd1f1be3ffa24a5210fd7566e3c5c0b235
Paula Andrea Cadena Moreno <pcadena@linux6.cs.uchicago.edu>
2024-03-04 15:35:20 -0600

    Access Data

--- gh/linkage/final_data.py
+++ gh/linkage/final_data.py
@@ -2,11 +2,11 @@ import pathlib
 from clean.clean_scrap import clean_scrap
 from linkage.match import match
 
-selected_npi = pathlib.Path(__file__).parent / "data/selected_columns.csv"
-selected_scrap = pathlib.Path(__file__).parent / "data/selected_columns_s.csv"
-scrap = ""
-npi = ""
-impact = ""
+selected_npi = pathlib.Path(__file__).parent / "selected_columns.csv"
+selected_scrap = pathlib.Path(__file__).parent / "selected_columns_s.csv"
+scrap = pathlib.Path(__file__).parent / "ccare_results_clean.json"
+npi = "https://drive.google.com/file/d/1XoFuh9wP999pa4kcbb-lCCTXJBqYzBBv/view?usp=share_link"
+impact = "https://drive.google.com/file/d/1GFP_fN-o-xVTBpv1EOZrKGqTf45KDKu3/view?usp=share_link"
 
 clean_scrap(scrap, selected_scrap).to_csv(
     pathlib.Path(__file__).parent / "data/county_care.csv", index=False
