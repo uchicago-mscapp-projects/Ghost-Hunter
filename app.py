8dac857773024570ef42d0496610ab8b3249ab2f
mmbarros-git <mmbarros@uchicago.edu>
2024-03-04 14:57:31 -0600

    data viz

--- app.py
+++ app.py
@@ -1,255 +1,166 @@
-# https://stackoverflow.com/questions/63459424/how-to-add-multiple-graphs-to-dash-app-on-a-single-browser-page
-
-
-# from dash import Dash, html, dcc
-# import plotly.express as px
-# import pandas as pd
-
-# # import dash
-# # import dash_core_components as dcc
-# # import dash_html_components as html
-# from dash.dependencies import Input, Output
-# # import pandas as pd
-# import plotly.express as px
-
-# external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-
-# app = Dash(__name__, external_stylesheets=external_stylesheets)
-
-# # assume you have a "long-form" data frame
-# # see https://plotly.com/python/px-arguments/ for more options
-# df_bar = pd.DataFrame({
-#     "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
-#     "Amount": [4, 1, 2, 2, 4, 5],
-#     "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
-# })
-
-# fig = px.bar(df_bar, x="Fruit", y="Amount", color="City", barmode="group")
-
-# app.layout = html.Div(children=[
-#     # All elements from the top of the page
-#     html.Div([
-#         html.H1(children='Hello Dash'),
-
-#         html.Div(children='''
-#             Dash: A web application framework for Python.
-#         '''),
-
-#         dcc.Graph(
-#             id='graph1',
-#             figure=fig
-#         ),  
-#     ]),
-#     # New Div for all elements in the new 'row' of the page
-#     html.Div([
-#         html.H1(children='Hello Dash'),
-
-#         html.Div(children='''
-#             Dash: A web application framework for Python.
-#         '''),
-
-#         dcc.Graph(
-#             id='graph2',
-#             figure=fig
-#         ),  
-#     ]),
-# ])
-
-# if __name__ == '__main__':
-#     app.run_server(debug=True)
-
-
-import dash
-import dash_core_components as dcc
-import dash_html_components as html
-from dash.dependencies import Input, Output
+#import libraries
 import pandas as pd
+from dash import Dash, html, dcc
 import plotly.express as px
+import urllib3
+import json
+import circlify
+import matplotlib.pyplot as plt
+import dash_bootstrap_components as dbc
 
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+#import graph
+from data_viz.viz_graph import bar_graph_providertype_match, bar_graph_providertype_prob_match, zicode_choropleth_graph
 
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
-# assume you have a "long-form" data frame
-# see https://plotly.com/python/px-arguments/ for more options
-df_bar = pd.DataFrame({
-    "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
-    "Amount": [4, 1, 2, 2, 4, 5],
-    "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
-})
 
-fig = px.bar(df_bar, x="Fruit", y="Amount", color="City", barmode="group")
+def create_card():
+    """
+    Returns (dbc.Card): A Dash Card that will be information box. 
+    """ 
 
-# Data for the tip-graph
-df_tip = px.data.tips()
+    prov_il = 1000
+    prov_medicaid = 500
+    prov_county_care = 100
+
+    
+    usa_medicaid = "18.8%"
+    illinois_medicaid = "30%"
+
+    total_scrape = 100
+    total_match = "50%"
+    total_nonmatch = "50%"
+
+
+    header_style = {"color": "#0092ca", 
+            "font-weight": "bold",
+            "background-color":"white",
+            "font-family" :"Roboto",
+            "text-align" : "center",
+            "font-size": "2rem"}
+    
+    body_style = {"color": "#757a79", 
+            "background-color":"white",
+            "font-family" :"Roboto",
+            "text-align" : "center",
+            "font-size": "1.5rem"}
+
+    card_content1 = [
+        dbc.CardHeader("TOTAL PROVIDERS", style=header_style),
+        dbc.CardBody(
+            [
+                html.P(f"Illinois {prov_il}", style=body_style, className="card-title"),
+                html.P(f"Accept Medicaid(Illinois) {prov_medicaid}", style=body_style,
+                    className="card-text",
+                ),
+                html.P(f"County Care (Medicaid) {prov_county_care}", style=body_style,
+                    className="card-text",
+                )
+            ]
+        ),
+    ]
+
+    card_content2 = [
+        dbc.CardHeader("MEDICAID PATIENTES", style=header_style),
+        dbc.CardBody(
+            [
+                html.P(f"USA {usa_medicaid}", style=body_style, className="card-title"),
+                html.P(f"Illinois {illinois_medicaid}", style=body_style,
+                    className="card-text",
+                ),
+            ]
+        ),
+    ]
+
+
+    card_content3 = [
+        dbc.CardHeader("COUNTY CARE SCRAPE",style=header_style),
+        dbc.CardBody(
+            [
+                html.P(f"Total scrape {total_scrape}", style=body_style, className="card-title"),
+                html.P(f"Match {total_match}", style=body_style,
+                    className="card-text",
+                ),
+                html.P(f"Non Match {total_nonmatch}", style=body_style,
+                    className="card-text",
+                )
+            ]
+        ),
+    ]
+
+    cards = html.Div(
+        [
+            dbc.Row(
+                [
+                    dbc.Col(dbc.Card(card_content1, color="#5dacbd", outline=True)),
+                    dbc.Col(
+                        dbc.Card(card_content2, color="#a7bcb9", outline=True)
+                    ),
+                    dbc.Col(dbc.Card(card_content3, color=" #e0ebeb", outline=True)),
+                ],
+                className="mb-4",
+            )
+        ]
+    )
+    
+    return cards
+
+
+
+app = Dash(__name__)
+
+abstract_text = '''# Project Abstract
+
+Our application audits the medicaid network of County Care, one of Illinois five 
+Medicaid Managed Care Organizations to see how what percentage of [County Care’s](https://countycare.valence.care/member/#findAProvider) 
+network can be independently verified. First, we scrape County Care’s find a 
+provider tool  to construct a database which presents a sample of their health care service provider 
+network.  Second we attempt to locate each provider listed in County Care’s network 
+in the State of Illinois’ [IMPACT data](https://ext2.hfs.illinois.gov/hfsindprovdirectory/) 
+which tracks all providers approved to bill Illinois Medicaid. Finally we attempt 
+to identify unmatched providers in the [National NPI Registry](https://www.cms.gov/medicare/regulations-guidance/administrative-simplification/data-dissemination)  
+which is a national data base of all health care providers. From this analysis we 
+estimate groups of providers that while listed in County Care’s directory are not 
+actually seeing or billing Medicaid patients, and whose presence in County Care’s 
+directory is misleading to patients and inflates the strength of their network. 
+Finally we examine these estimates against sub-groups of medical providers and across 
+zip codes to look for trends across specialities and geography.'''
+
+abstract = dcc.Markdown(abstract_text, style={"color": "#24527a",
+                   "background-color":"#ececec",
+                   "font-family" :"Roboto",
+                   "text-align" : "center",
+                   "font-size": "1rem"})
+info_cards = create_card() 
+graph1 = bar_graph_providertype_match('Matches by Provider Type', '#9fd3c7', '#007cb9')
+graph2 = bar_graph_providertype_prob_match('Probability of Macth for Provider Type','#9fd3c7')
+graph3 = zicode_choropleth_graph(df)
+
+title = html.H1(children='Ghost Hunter', 
+            style={"color": "#24527a", 
+                   "font-weight": "bold",
+                   "background-color":"white",
+                   "font-family" :"Roboto",
+                   "text-align" : "center",
+                   "font-size": "8rem"})
+
+graph_title = html.H3(children='County Care Scrape', 
+            style={"color": "#007cb9", 
+                   "font-weight": "bold",
+                   "background-color":"white",
+                   "font-family" :"Roboto",
+                   "text-align" : "center",
+                   "font-size": "4rem"})
 
 app.layout = html.Div(children=[
-    # All elements from the top of the page
-    html.Div([
-        html.H1(children='Hello Dash'),
-
-        html.Div(children='''
-            Dash: A web application framework for Python.
-        '''),
-
-        dcc.Graph(
-            id='example-graph',
-            figure=fig
-        ),  
-    ]),
-    # New Div for all elements in the new 'row' of the page
-    html.Div([ 
-        dcc.Graph(id='tip-graph'),
-        html.Label([
-            "colorscale",
-            dcc.Dropdown(
-                id='colorscale-dropdown', clearable=False,
-                value='bluyl', options=[
-                    {'label': c, 'value': c}
-                    for c in px.colors.named_colorscales()
-                ])
-        ]),
-    ])
+    title,
+    abstract,
+    info_cards,
+    graph_title, 
+    graph1,
+    graph2,
+    graph3
+    
 ])
 
-# Callback function that automatically updates the tip-graph based on chosen colorscale
-@app.callback(
-    Output('tip-graph', 'figure'),
-    [Input("colorscale-dropdown", "value")]
-)
-def update_tip_figure(colorscale):
-    return px.scatter(
-        df_color, x="total_bill", y="tip", color="size",
-        color_continuous_scale=colorscale,
-        render_mode="webgl", title="Tips"
-    )
-
 if __name__ == '__main__':
-    app.run_server(debug=True)
-
-
-# # Run this app with `python app.py` and
-# # visit http://127.0.0.1:8050/ in your web browser.
-
-
-# from dash import Dash, html, dcc
-# import plotly.express as px
-# import pandas as pd
-
-# app = Dash(__name__)
-
-# # assume you have a "long-form" data frame
-# # see https://plotly.com/python/px-arguments/ for more options
-# df = pd.DataFrame({
-#     "Provider Type": ["Behavioral Health", "Hospitals", "Surgery", "Surgery", "Hospitals", "Behavioral Health"],
-#     "Amount": [4, 1, 2, 2, 4, 5],
-#     "Match": ["Total", "Total", "Total", "Match", "Match", "Match"]
-
-
-# })
-
-# fig = px.bar(df, x="Provider Type", y="Amount", color="Match", barmode="group")
-
-# app.layout = html.Div(children=[
-#     html.H1(children='Ghost Hunters'),
-
-#     html.Div(children='''
-#         Dash: A web application framework for your data.
-#     '''),
-
-#     dcc.Graph(
-#         id='example-graph',
-#         figure=fig
-#     )
-    
-# ])
-
-# if __name__ == '__main__':
-#     app.run(debug=True)
-
-# Run this app with `python app.py` and
-# visit http://127.0.0.1:8050/ in your web browser.
-
-
-# from dash import Dash, html
-# import pandas as pd
-
-# df2 = pd.read_csv('https://gist.githubusercontent.com/chriddyp/c78bf172206ce24f77d6363a2d754b59/raw/c353e8ef842413cae56ae3920b8fd78468aa4cb2/usa-agricultural-exports-2011.csv')
-
-
-# def generate_table(dataframe, max_rows=10):
-#     return html.Table([
-#         html.Thead(
-#             html.Tr([html.Th(col) for col in dataframe.columns])
-#         ),
-#         html.Tbody([
-#             html.Tr([
-#                 html.Td(dataframe.iloc[i][col]) for col in dataframe.columns
-#             ]) for i in range(min(len(dataframe), max_rows))
-#         ])
-#     ])
-
-
-# app = Dash(__name__)
-
-# app.layout = html.Div([
-#     html.H4(children='US Agriculture Exports (2011)'),
-#     generate_table(df2)
-# ])
-
-# if __name__ == '__main__':
-#     app.run(debug=True)
-
-# Run this app with `python app.py` and
-# visit http://127.0.0.1:8050/ in your web browser.
-
-
-# from dash import Dash, dcc, html
-# import plotly.express as px
-# import pandas as pd
-
-
-# app = Dash(__name__)
-
-# df = pd.read_csv('https://gist.githubusercontent.com/chriddyp/5d1ea79569ed194d432e56108a04d188/raw/a9f9e8076b837d541398e999dcbac2b2826a81f8/gdp-life-exp-2007.csv')
-
-# fig = px.scatter(df, x="gdp per capita", y="life expectancy",
-#                  size="population", color="continent", hover_name="country",
-#                  log_x=True, size_max=60)
-
-# app.layout = html.Div([
-#     dcc.Graph(
-#         id='life-exp-vs-gdp',
-#         figure=fig
-#     )
-# ])
-
-# if __name__ == '__main__':
-#     app.run(debug=True)
-
-# Run this app with `python app.py` and
-# visit http://127.0.0.1:8050/ in your web browser.
-
-
-# from dash import Dash, html, dcc
-
-# app = Dash(__name__)
-
-# markdown_text = '''
-# ### Dash and Markdown
-
-# Dash apps can be written in Markdown.
-# Dash uses the [CommonMark](http://commonmark.org/)
-# specification of Markdown.
-# Check out their [60 Second Markdown Tutorial](http://commonmark.org/help/)
-# if this is your first introduction to Markdown!
-# '''
-
-# app.layout = html.Div([
-#     dcc.Markdown(children=markdown_text)
-# ])
-
-# if __name__ == '__main__':
-#     app.run(debug=True)
-
-
+    app.run(debug=True)
