56f4ea57bb6ecb6a711eccd5e74bf461bef51ef7
paula-cadena <85127068+paula-cadena@users.noreply.github.com>
2024-03-06 23:04:22 -0600

    Update Final Data

--- gh/linkage/final_data.py
+++ gh/linkage/final_data.py
@@ -4,10 +4,13 @@ from linkage.match import match
 
 selected_npi = pathlib.Path(__file__).parent / "selected_columns.csv"
 selected_scrap = pathlib.Path(__file__).parent / "selected_columns_s.csv"
-scrap = pathlib.Path(__file__).parent / "ccare_results_clean.json"
-npi = "https://drive.google.com/file/d/1XoFuh9wP999pa4kcbb-lCCTXJBqYzBBv/view?usp=share_link"
-impact = "https://drive.google.com/file/d/1GFP_fN-o-xVTBpv1EOZrKGqTf45KDKu3/view?usp=share_link"
 
+#Because of size, not available at Git
+scrap = 'local'
+npi = 'local'
+impact = 'local'
+
+#Data used for visualization
 clean_scrap(scrap, selected_scrap).to_csv(
     pathlib.Path(__file__).parent / "data/county_care.csv", index=False
 )
