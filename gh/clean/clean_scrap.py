c86ad1ab0cc669862f660c90f68bc9062c7a6130
paula-cadena <85127068+paula-cadena@users.noreply.github.com>
2024-03-06 23:02:20 -0600

    Update Clean Scrap

--- gh/clean/clean_scrap.py
+++ gh/clean/clean_scrap.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import json
 
-columns_to_match = [
+COLUMNS_TO_MATCH = [
     "first_name",
     "last_name",
     "phone_number",
@@ -9,16 +9,23 @@ columns_to_match = [
     "address",
 ]
 
-
 def clean_scrap(json_path, columns_path):
+    """
+    Clean and preprocess scrap data (CountyCare) from a JSON file based on selected columns.
 
+    Parameters:
+    - json_path (str): Path to the JSON file containing scrap data.
+    - columns_path (str): Path to the CSV file specifying selected columns.
+
+    Returns:
+    - DataFrame: Cleaned and preprocessed DataFrame with selected columns and standardized phone numbers.
+    """
     with open(json_path, "r") as file:
         data = json.load(file)
     selected_columns = pd.read_csv(columns_path)
     df = pd.DataFrame(data)[selected_columns["var"].tolist()]
 
     # Read file only with selected columns
-
     column_mapping = dict(zip(selected_columns["var"], selected_columns["new_names"]))
 
     df.rename(columns=column_mapping, inplace=True)
@@ -29,15 +36,26 @@ def clean_scrap(json_path, columns_path):
             lambda x: isinstance(x, str) and (len(x) != 10 or len(set(x)) == 1), None
         )
     )
-
+    with open('total.json','w') as f:
+        json.dumps(len(df),f)
+    
     # Drop duplicates
     df.drop_duplicates(inplace=True)
 
     return df
 
-
 def match_scrap(scrap, columns):
-    df = clean_scrap(scrap, columns)[columns_to_match]
+    """
+    Extract and match relevant columns from preprocessed scrap data.
+
+    Parameters:
+    - scrap (str): Path to the JSON file containing scrap data.
+    - columns (str): Path to the CSV file specifying selected columns.
+
+    Returns:
+    - DataFrame: Extracted and matched DataFrame with relevant columns from scrap data.
+    """
+    df = clean_scrap(scrap, columns)[COLUMNS_TO_MATCH]
     df.dropna(inplace=True)
     df.drop_duplicates(inplace=True)
 
