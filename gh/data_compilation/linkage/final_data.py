b3761eb893e2745284ce5b2bd2b33e6e836432c6
Paula Cadena <paulacadena@Mac.attlocal.net>
2025-02-11 17:17:13 -0600

    Add final_data.py (Runs clean & linkage)

---
+++ gh/data_compilation/linkage/final_data.py
@@ -0,0 +1,19 @@
+import pathlib
+from clean.clean_scrap import clean_scrap
+from .match import match
+
+selected_npi = pathlib.Path(__file__).parent / "selected_columns.csv"
+selected_scrap = pathlib.Path(__file__).parent / "selected_columns_s.csv"
+
+#Because of size, not available at GitHub
+scrap = 'local'
+npi = 'local'
+impact = 'local'
+
+#Data used for visualization
+clean_scrap(scrap, selected_scrap).to_csv(
+    pathlib.Path(__file__).parent / "data/county_care.csv", index=False
+)
+match(npi, scrap, impact, selected_npi, selected_scrap).to_csv(
+    pathlib.Path(__file__).parent / "data/merge.csv", index=False
+)
